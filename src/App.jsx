84a705af71ae9c8f34b91fadbaed8d26bf293c69
Ahgama sundar <ahgamasundar40@gmail.com>
2025-08-09 12:34:53 +0530

    first commit and nav bar

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Navbar from "./sections/navbar.jsx";
+const App = () => {
+    return (
+        <div>
+            <Navbar />
+        </div>
+    );
+}
+export default App;
---
+++ src/sections/navbar.jsx
@@ -0,0 +1,44 @@
+import { useState } from "react";
+import {navLinks} from "../constants/index.js";
+
+const Navitems=() =>{
+    return(
+        <ul className="nav-ul">
+           {navLinks.map(({id,href,name})=>(
+            <li key={id} className="nav-li">
+                <a href={href}>{name}</a>
+            </li>
+
+           ))}
+        </ul>
+
+    )
+}
+
+
+const Navbar =()=> {
+    const [isOpen,setIsopen]=useState(false);
+    const handleIsopen =()=>{
+        setIsopen((Previsopen)=> !Previsopen);
+    }
+    return (
+      <header className="fixed top-0 left-0 right-0 z-50 bg-black/90">
+        <div className="max-w-7xl mx-auto">
+            <div className=" flex justify-between items-center py-5 mx-auto c-space">
+                <a href = "/" className="text-neutral-400 font-bold text-xl hover:text-white transition-colors">Test</a>
+                <button onClick={handleIsopen} className="text-neutral-400 hover:text-white focus:outline-none sm:hidden flex 
+                area" aria-label="Toggle menu">
+                    <img src={isOpen ?'assets/close.svg':'assets/menu.svg'} alt="toggle" className="w-7 h-7 "/>
+                </button>
+
+                <nav className="sm:flex hidden">
+                    <Navitems />
+                    </nav> 
+            </div>
+
+        </div>
+
+      </header>
+    );
+    } 
+export default Navbar;
