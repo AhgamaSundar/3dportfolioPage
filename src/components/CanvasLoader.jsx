cf4482233c919133b2e55d9fbc3f031a4f411d72
Ahgama sundar <ahgamasundar40@gmail.com>
2025-08-12 01:14:15 +0530

    mesh positioned

--- src/components/CanvasLoader.jsx
+++ src/components/CanvasLoader.jsx
@@ -6,7 +6,7 @@ const CanvasLoader = () => {
         <Html as="div" center style={{ display: 'flex', alignItems: 'center', flexDirection: 'column' }}> 
             <span className="canvas-loader" />
             <p style={{fontSize: 14, color: '#F1F1F1', marginTop: 40 ,fontWeight:800}}>
-                {progress != 0  ? `${progress.toFixed(2)}%`:'Loading...'} </p>
+                {progress != 0  ? `${progress.toFixed(1)}%`:'Loading...'} </p>
         </Html>
     );
 }       
--- src/sections/hero.jsx
+++ src/sections/hero.jsx
@@ -1,29 +1,48 @@
 import { PerspectiveCamera } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
+import { Suspense } from "react";
+import { OrbitControls } from "three-stdlib";
+import { Leva,useControls } from "leva";
+import {useEffect} from "react";
 import CanvasLoader  from "../components/CanvasLoader.jsx";
 import HackerRoom from "../components/Hackerroom.jsx";
-import { Suspense } from "react";
-import React from "react";
-import { OrbitControls } from "three-stdlib";
+import { calculateSizes } from "../constants/index.js";
+import { useMediaQuery } from "react-responsive";
 const Hero = () => {
+    const isSmall = useMediaQuery({ maxWidth: 440 });
+     const isTablet = useMediaQuery({ minwidth:768,maxWidth: 1024 });
+      const isMobile = useMediaQuery({ maxWidth: 768 });
+    const sizes =calculateSizes(isSmall, isTablet, isMobile);
+    const dlc = useControls('DirectionalLight',{
+        positionX:{value:0, min:-10, max:10, step:0.01},
+        positionY:{value:0, min:-10, max:10, step:0.01},
+        positionZ:{value:0, min:-10, max:10, step:0.01},
+        intensity:{value:1, min:0, max:10, step:0.01}
+    });        
+   
+ 
     return (
-        <section className="min-h-screen w-full flex flex-col relative " >
+        <section className="min-h-screen w-full flex flex-col  " >
             <div className="w-full mx-auto flex flex-col sm:mt-36 mt-20 c-space gap-3">
                 <p className="sm:text-3xl text-xl text-white text-center font-medium font-generalsans">Hey ,I am Ahgama <span className="waving-hand">👋</span></p>
                 <p className="hero_tag text-gray_gradient">Trying to survive</p>
             </div>
-            <div className="w-full h-full  inset-0"> 
+            <div className="w-full h-full absolute inset-0"> 
+                <Leva />
             <Canvas className="w-full h-full">
                 <Suspense fallback={<CanvasLoader />}> 
-                <PerspectiveCamera makeDefault position={[0, 0,30]   } />
                
-                <HackerRoom  scale={0.1} position={[0,0,0]} rotation={[0,92,0]}/>
+                <HackerRoom  scale={sizes.deskScale} 
+                position={sizes.deskPosition} 
+                rotation={[0,-Math.PI,0]}/>
                 <ambientLight intensity={1} />
-                <directionalLight position={[10, 10,10]} intensity={1} />
+                <directionalLight position={[dlc.positionX,dlc.positionY,dlc.positionZ]} intensity={dlc.intensity} />
                 
+                <PerspectiveCamera makeDefault position={[0, 0,20]   } />
                  </Suspense>
 
             </Canvas>
+            
             </div>
             </section>
     );
