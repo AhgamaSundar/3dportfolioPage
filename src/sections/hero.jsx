ce6c9602c7ede0f0dfd5e61114061a07396866ef
Ahgama sundar <ahgamasundar40@gmail.com>
2025-08-11 16:30:10 +0530

    basic mesh added

--- src/sections/hero.jsx
+++ src/sections/hero.jsx
@@ -1,4 +1,10 @@
+import { PerspectiveCamera } from "@react-three/drei";
+import { Canvas } from "@react-three/fiber";
+import CanvasLoader  from "../components/CanvasLoader.jsx";
+import HackerRoom from "../components/Hackerroom.jsx";
+import { Suspense } from "react";
 import React from "react";
+import { OrbitControls } from "three-stdlib";
 const Hero = () => {
     return (
         <section className="min-h-screen w-full flex flex-col relative " >
@@ -6,7 +12,19 @@ const Hero = () => {
                 <p className="sm:text-3xl text-xl text-white text-center font-medium font-generalsans">Hey ,I am Ahgama <span className="waving-hand">👋</span></p>
                 <p className="hero_tag text-gray_gradient">Trying to survive</p>
             </div>
-            <div className="w-full h-full absolute inset-0"> </div>
+            <div className="w-full h-full  inset-0"> 
+            <Canvas className="w-full h-full">
+                <Suspense fallback={<CanvasLoader />}> 
+                <PerspectiveCamera makeDefault position={[0, 0,30]   } />
+               
+                <HackerRoom  scale={0.1} position={[0,0,0]} rotation={[0,92,0]}/>
+                <ambientLight intensity={1} />
+                <directionalLight position={[10, 10,10]} intensity={1} />
+                
+                 </Suspense>
+
+            </Canvas>
+            </div>
             </section>
     );
 }
