2af8237231bc12cfe0fab832f08c6dc945800c77
Ahgama sundar <ahgamasundar40@gmail.com>
2025-08-21 14:43:17 +0530

    Added contect section

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Hero from "./sections/hero.jsx";
 import Navbar from "./sections/navbar.jsx";
 import About from './sections/About.jsx';
 import Project from './sections/Project.jsx';
+import Contact from './sections/Contact.jsx';
 const App = () => {
     return (
         <main className="max-w-7xl mx-auto">
@@ -10,6 +11,7 @@ const App = () => {
             <Hero />
             <About />
             <Project />
+            <Contact />
             
         </main>
     );
---
+++ src/sections/Contact.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Contact = () => {
+    return (
+        <section className=' c-space my-20'>
+            <div className='relative min-h-screen flex items-center justify-center flex-col'>
+            <img src='/assets/terminal.png' al="terminal" className='absolute inset -0 min-h-screen' />    
+           </div>
+            <h3 className='head-text'>Contact Me</h3>
+        </section>
+    );
+};
+
+export default Contact;
